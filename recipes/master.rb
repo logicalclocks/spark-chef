f5e20625d65c8daa4b9f169fb2c7ef1892c056e1
Jim Dowling <jdowling@kth.se>
2015-02-20 12:25:19 +0100

    spark master fix

--- recipes/master.rb
+++ recipes/master.rb
@@ -9,12 +9,12 @@ hadoop_hdfs_directory "/user/#{node[:spark][:user]}/share/lib" do
   mode "755"
 end
 
-hadoop_hdfs_directory "#{node[:spark][:home]}/assembly/lib/spark-assembly_*.jar" do
-  action :put
-  dest "/user/#{node[:spark][:user]}/share/lib/spark-assembly.jar "
-  owner node[:spark][:user]
-  mode "0755"
-end
+#hadoop_hdfs_directory "#{node[:spark][:home]}/assembly/lib/spark-assembly_*.jar" do
+#  action :put
+#  dest "/user/#{node[:spark][:user]}/share/lib/spark-assembly.jar "
+#  owner node[:spark][:user]
+#  mode "0755"
+#end
 
 # hdfs dfs -mkdir -p /user/spark/share/lib 
 # hdfs dfs -put $SPARK_HOME/assembly/lib/spark-assembly_*.jar  \     
