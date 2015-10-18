2dd0a38b1b1a2c4d03c77a12843e7efdcb561db1
Jim Dowling <jdowling@sics.se>
2015-10-18 07:38:45 +0200

    worker recipe added. Script still called start-slave.sh

--- providers/start.rb
+++ providers/start.rb
@@ -21,12 +21,12 @@ action :start_worker do
 
     cd #{node[:spark][:home]}    
 # Spark 1.4.x
-#    ./sbin/start-worker.sh #{new_resource.master_url}
+#    ./sbin/start-slave.sh #{new_resource.master_url}
 # Spark 1.3.x
-    ./sbin/start-worker.sh #{new_resource.worker_id} #{new_resource.master_url}
+    ./sbin/start-slave.sh #{new_resource.worker_id} #{new_resource.master_url}
     EOF
     not_if "jps | grep Worker"
-#    not_if "#{node[:spark][:home]}/sbin/start-worker.sh --properties-file #{node[:spark][:home]}/conf/spark-defaults.conf | grep \"stop it first\""
+#    not_if "#{node[:spark][:home]}/sbin/start-slave.sh --properties-file #{node[:spark][:home]}/conf/spark-defaults.conf | grep \"stop it first\""
   end
  
 end
---
+++ recipes/worker.rb
@@ -0,0 +1,33 @@
+
+master_ip = private_recipe_ip("spark","master")
+
+# Generate a unique id for each worker using the list of ip addresses
+# for the workers.
+# Get the ip address of this worker. Then find the offset of this worker in the
+# list of private_ips set by karamel. The offset of 'my_ip' is the worker_id.
+my_ip = my_private_ip()
+found_id = -1
+id = 1
+for worker in node[:spark][:worker][:private_ips]
+  if my_ip.eql? worker
+    Chef::Log.info "Found matching IP address in the list of nodes: #{worker} . ID= #{id}"
+    found_id = id
+  end
+  id += 1
+end 
+
+if found_id == -1
+   Chef::Log.fatal "Could not find matching IP address in list the spark workers."
+end
+
+
+spark_start "spark://#{master_ip}:#{node[:spark][:master][:port]}" do
+  worker_id found_id
+  action :start_worker
+end
+
+homedir = node[:spark][:user].eql?("root") ? "/root" : "/home/#{node[:spark][:user]}"
+
+spark_master "#{homedir}" do
+  action :get_publickey
+end
