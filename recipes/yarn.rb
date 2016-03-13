e466d6eaa489f03483264e44f8affc3eed6e93ff
%github_username% <jdowling@sics.se>
2016-03-13 22:34:42 +0100

    spark.jar copy in

--- recipes/yarn.rb
+++ recipes/yarn.rb
@@ -28,5 +28,5 @@ apache_hadoop_hdfs_directory "#{node.hadoop_spark.home}/lib/spark-assembly-#{nod
   owner node.hadoop_spark.user
   group node.apache_hadoop.group
   mode "1755"
-  dest "#{home}/#{node.hadoop_spark.user}/hadoop_spark.jar"
+  dest "#{home}/#{node.hadoop_spark.user}/spark.jar"
 end
