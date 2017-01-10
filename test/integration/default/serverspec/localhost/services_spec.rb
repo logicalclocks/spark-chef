c456721695611590668776d17dcb8002cd45bd0a
Jim Dowling <jdowling@sics.se>
2017-01-10 20:38:36 +0100

    upgrade to 2.1.0

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -71,11 +71,11 @@ describe command("service sparkhistoryserver restart") do
 end
 
 
-describe command("su spark -l -c \"HADOOP_CONF_DIR=/srv/hadoop/etc/hadoop /srv/spark/bin/spark-submit --verbose --class org.apache.spark.examples.SparkPi --master yarn --deploy-mode client --driver-memory 512m --executor-memory 512m --queue default --num-executors 1 /srv/spark/examples/jars/spark-examples_2.11-2.0.1.jar 100\"") do
+describe command("su spark -l -c \"HADOOP_CONF_DIR=/srv/hadoop/etc/hadoop /srv/spark/bin/spark-submit --verbose --class org.apache.spark.examples.SparkPi --master yarn --deploy-mode client --driver-memory 512m --executor-memory 512m --queue default --num-executors 1 /srv/spark/examples/jars/spark-examples_2.11-2.1.0.jar 100\"") do
   its (:stdout) { should match /Pi is roughly/ }
 end
 
 # When you run in cluster-mode, spark-submit works ok, but doesn't return, so this fails
-describe command("su spark -l -c \"HADOOP_CONF_DIR=/srv/hadoop/etc/hadoop /srv/spark/bin/spark-submit --verbose --class org.apache.spark.examples.SparkPi --master yarn --deploy-mode cluster --driver-memory 512m --executor-memory 512m --queue default --num-executors 1 /srv/spark/examples/jars/spark-examples_2.11-2.0.1.jar 100\"") do
+describe command("su spark -l -c \"HADOOP_CONF_DIR=/srv/hadoop/etc/hadoop /srv/spark/bin/spark-submit --verbose --class org.apache.spark.examples.SparkPi --master yarn --deploy-mode cluster --driver-memory 512m --executor-memory 512m --queue default --num-executors 1 /srv/spark/examples/jars/spark-examples_2.11-2.1.0.jar 100\"") do
   its(:exit_status) { should eq 0 }
 end
