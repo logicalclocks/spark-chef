a192c2633227ef203d2f4106fb49d77de5373f57
Jim Dowling <jdowling@sics.se>
2017-08-04 09:43:41 +0200

    syntax error

--- recipes/yarn.rb
+++ recipes/yarn.rb
@@ -214,8 +214,8 @@ jarFile="spark-#{node['hadoop_spark']['version']}-yarn-shuffle.jar"
 remote_file "Copy spark yarn shuffle jar file" do
   path "#{node['hops']['base_dir']}/share/hadoop/yarn/lib/#{jarFile}"
   source "#{node['hadoop_spark']['base_dir']/yarn/#{jarFile}"
-  owner "#{node['hops']['yarn']['user']}"
-  group "#{node['hops']['group']}"
+  owner node['hops']['yarn']['user']
+  group node['hops']['group']
   mode 0644
   action :create_if_missing  
 end
