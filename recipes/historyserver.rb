328c9005d89e41db2a7b98da094e81a971d30374
Jim Dowling <jdowling@sics.se>
2017-08-07 15:25:21 +0200

    change spark-history-server logfile for kagent

--- recipes/historyserver.rb
+++ recipes/historyserver.rb
@@ -80,10 +80,8 @@ end
 if node.kagent.enabled == "true" 
    kagent_config "sparkhistoryserver" do
      service service_name
-     log_file "#{node.hadoop_spark.base_dir}/historyserver.log"
+     log_file "#{node.hadoop_spark.base_dir}/logs/spark-#{node['hadoop_spark']['user']}-org.apache.spark.deploy.history.HistoryServer-1-#{node['hostname']}.out
      web_port node.hadoop_spark.historyserver.port
    end
 end
-
-
 
