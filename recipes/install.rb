12b31d3c0e2f2f6745e5ef0fecfbdc3e604ed402
Theofilos Kakantousis <t.kakantousis@gmail.com>
2017-12-05 16:27:44 +0100

    add spark hive jar

--- recipes/install.rb
+++ recipes/install.rb
@@ -75,7 +75,7 @@ end
 purl=node['hadoop_spark']['spark-sql-dependencies_url']
 # The following dependencies are required to run spark-sql with parquet and orc. We install them here so that users don't have to do it from their notebooks/jobs
 # https://mvnrepository.com/artifact/org.spark-project.hive/hive-exec/1.2.1.spark2
-files= %w{ parquet-encoding-1.9.0.jar parquet-common-1.9.0.jar parquet-hadoop-1.9.0.jar parquet-jackson-1.9.0.jar parquet-column-1.9.0.jar parquet-format-2.3.1.jar hive-exec-1.2.1.spark2.jar}
+files= %w{ parquet-encoding-1.9.0.jar parquet-common-1.9.0.jar parquet-hadoop-1.9.0.jar parquet-jackson-1.9.0.jar parquet-column-1.9.0.jar parquet-format-2.3.1.jar hive-exec-1.2.1.spark2.jar spark-hive_2.11-2.2.0.jar }
 
 for f in files do
 
