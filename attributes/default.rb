3b662d7edd19fe19e1957ae7a11237376f5cf0b6
Fabio Buso <dev.siroibaf@gmail.com>
2018-01-31 16:04:39 +0100

    Start influxdb before the namenode

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 include_attribute "kagent"
 include_attribute "hops"
+include_attribute "hopsmonitor"
 
 default['hadoop_spark']['user']                                 = node['install']['user'].empty? ? "spark" : node['install']['user']
 default['hadoop_spark']['group']                                = node['install']['user'].empty? ? node['hops']['group'] : node['install']['user']
