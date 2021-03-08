3a1b67aaa2628b167a52529d6e6ac653b098e7af
Mahmoud Ismail <mahmoud@logicalclocks.com>
2021-03-08 13:56:34 +0100

    [HOPSWORKS-CLOUD-1656] Missing tour files after upgrade (#123)
    * [HOPSWORKS-CLOUD-1656] Missing tour files after upgrade
    
    * fixes for review
    
    * refactor
    
    * refactor

--- recipes/yarn.rb
+++ recipes/yarn.rb
@@ -49,10 +49,60 @@ template"#{node['hadoop_spark']['home']}/conf/spark-defaults.conf" do
 end
 
 
+hopsVerification = File.basename(node['hadoop_spark']['hops_verification']['url'])
+hopsExamplesSpark=File.basename(node['hadoop_spark']['hopsexamples_spark']['url'])
+hopsExamplesFeaturestoreTour=File.basename(node['hadoop_spark']['hopsexamples_featurestore_tour']['url'])
+hsfs_utils = File.basename(node['hadoop_spark']['hsfs']['utils']['download_url'])
+
+is_head_node = false
+if exists_local("hopsworks", "default") and exists_local("cloud", "default")
+  unmanaged = false
+  if node.attribute? 'cloud' and node['cloud'].attribute? 'init' and node['cloud']['init'].attribute? 'config' and node['cloud']['init']['config'].attribute? 'unmanaged'
+    unmanaged = node['cloud']['init']['config']['unmanaged'].casecmp? 'true'
+  end
+  is_head_node = !unmanaged
+end
+
+is_first_spark_yarn_to_run = private_ip.eql?(node['hadoop_spark']['yarn']['private_ips'].sort[0])
+
+if is_head_node || is_first_spark_yarn_to_run
+  remote_file "#{Chef::Config['file_cache_path']}/#{hopsVerification}" do
+    source node['hadoop_spark']['hops_verification']['url']
+    owner node['hadoop_spark']['user']
+    group node['hops']['group']
+    mode "0755"
+    action :create
+  end
+
+  remote_file "#{Chef::Config['file_cache_path']}/#{hopsExamplesSpark}" do
+    source node['hadoop_spark']['hopsexamples_spark']['url']
+    owner node['hadoop_spark']['user']
+    group node['hops']['group']
+    mode "1755"
+    action :create
+  end
+
+  remote_file "#{Chef::Config['file_cache_path']}/#{hopsExamplesFeaturestoreTour}" do
+    source node['hadoop_spark']['hopsexamples_featurestore_tour']['url']
+    owner node['hadoop_spark']['user']
+    group node['hops']['group']
+    mode "1755"
+    action :create
+  end
+
+  remote_file "#{Chef::Config['file_cache_path']}/#{hsfs_utils}" do
+    source node['hadoop_spark']['hsfs']['utils']['download_url']
+    owner node['hadoop_spark']['user']
+    group node['hops']['group']
+    mode "1755"
+    action :create
+  end
+end 
+
 # Only the first of the spark::yarn hosts needs to run this code (not all of them)
 #see HOPSWORKS-572 why the following if clause changed
 #if private_ip.eql? node['hadoop_spark']['yarn']['private_ips'][0]
-if (private_ip.eql?(node['hadoop_spark']['yarn']['private_ips'].sort[0]))
+if is_first_spark_yarn_to_run
 
   hops_hdfs_directory "#{home}" do
     action :create_as_superuser
@@ -98,15 +148,6 @@ if (private_ip.eql?(node['hadoop_spark']['yarn']['private_ips'].sort[0]))
     end
   end
 
-  hopsVerification = File.basename(node['hadoop_spark']['hops_verification']['url'])
-  remote_file "#{Chef::Config['file_cache_path']}/#{hopsVerification}" do
-    source node['hadoop_spark']['hops_verification']['url']
-    owner node['hadoop_spark']['user']
-    group node['hops']['group']
-    mode "0755"
-    action :create
-  end
-
   hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{hopsVerification}" do
     owner node['hadoop_spark']['user']
     group node['hops']['group']
@@ -115,30 +156,12 @@ if (private_ip.eql?(node['hadoop_spark']['yarn']['private_ips'].sort[0]))
     action :replace_as_superuser
   end
 
-  hopsExamplesSpark=File.basename(node['hadoop_spark']['hopsexamples_spark']['url'])
-  remote_file "#{Chef::Config['file_cache_path']}/#{hopsExamplesSpark}" do
-    source node['hadoop_spark']['hopsexamples_spark']['url']
-    owner node['hadoop_spark']['user']
-    group node['hops']['group']
-    mode "1755"
-    action :create
-  end
-
   hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{hopsExamplesSpark}" do
     action :replace_as_superuser
     owner node['hadoop_spark']['user']
     group node['hops']['group']
     mode "1755"
     dest "/user/#{node['hadoop_spark']['user']}/#{hopsExamplesSpark}"
-  end
-
-  hopsExamplesFeaturestoreTour=File.basename(node['hadoop_spark']['hopsexamples_featurestore_tour']['url'])
-  remote_file "#{Chef::Config['file_cache_path']}/#{hopsExamplesFeaturestoreTour}" do
-    source node['hadoop_spark']['hopsexamples_featurestore_tour']['url']
-    owner node['hadoop_spark']['user']
-    group node['hops']['group']
-    mode "1755"
-    action :create
   end
 
   hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{hopsExamplesFeaturestoreTour}" do
@@ -155,16 +178,6 @@ if (private_ip.eql?(node['hadoop_spark']['yarn']['private_ips'].sort[0]))
     group node['hops']['group']
     mode "1755"
     dest "/user/#{node['hadoop_spark']['user']}/log4j.properties"
-  end
-
-
-  hsfs_utils = File.basename(node['hadoop_spark']['hsfs']['utils']['download_url'])
-  remote_file "#{Chef::Config['file_cache_path']}/#{hsfs_utils}" do
-    source node['hadoop_spark']['hsfs']['utils']['download_url']
-    owner node['hadoop_spark']['user']
-    group node['hops']['group']
-    mode "1755"
-    action :create
   end
 
   hops_hdfs_directory "#{Chef::Config['file_cache_path']}/#{hsfs_utils}" do
@@ -202,4 +215,30 @@ link "#{node['hops']['base_dir']}/share/hadoop/yarn/lib/#{jarFile}" do
   owner node['hops']['yarn']['user']
   group node['hops']['group']
   to "#{node['hadoop_spark']['base_dir']}/yarn/#{jarFile}"
+end
+
+if is_head_node
+  hops_tours "Cache tour files locally" do 
+    action :update_local_cache
+    paths [
+            "#{Chef::Config['file_cache_path']}/#{hopsExamplesSpark}", 
+            "#{Chef::Config['file_cache_path']}/#{hopsExamplesFeaturestoreTour}", 
+            "#{Chef::Config['file_cache_path']}/#{hsfs_utils}",
+            "#{Chef::Config['file_cache_path']}/#{hopsVerification}",
+            "#{node['hadoop_spark']['home']}/conf/log4j.properties",
+            "#{node['hadoop_spark']['home']}/conf/hive-site.xml"
+          ]
+    hdfs_paths [
+                  "/user/#{node['hadoop_spark']['user']}/#{hopsExamplesSpark}", 
+                  "/user/#{node['hadoop_spark']['user']}/#{hopsExamplesFeaturestoreTour}",
+                  "/user/#{node['hadoop_spark']['user']}/#{hopsExamplesFeaturestoreTour}", 
+                  "/user/#{node['hadoop_spark']['user']}/#{hsfs_utils}", 
+                  "/user/#{node['hadoop_spark']['user']}/#{hopsVerification}",
+                  "/user/#{node['hadoop_spark']['user']}/log4j.properties",
+                  "/user/#{node['hadoop_spark']['user']}/hive-site.xml"
+                ]  
+    owner node['hadoop_spark']['user']
+    group node['hops']['group']
+    mode "1755"
+  end 
 end
