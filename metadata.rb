214378d1cbe47ddb35fa4734ce47221976e5caaf
Antonios Kouzoupis <antonios@logicalclocks.com>
2021-04-30 15:33:49 +0200

    [HOPSWORKS-2523] Use LC java cookbook

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.3.0"
 source_url       "https://github.com/hopshadoop/spark-chef"
 
-depends 'java', '~> 7.0.0'
 depends 'magic_shell', '~> 1.0.0'
 depends 'conda'
 depends 'kagent'
@@ -15,6 +14,7 @@ depends 'ndb'
 depends 'hops'
 depends 'hive2'
 depends 'hopsmonitor'
+depends 'java'
 
 recipe           "install", "Installs Spark binaries"
 #link:<a target='_blank' href='http://%host%:8080/'>Launch the WebUI for the Spark Master</a>
